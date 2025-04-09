7609d864f4a74f2ca97331de2a95e21ef66746bb
Gabriel D. Couto <gabrieldocoutos@gmail.com>
2025-04-09 19:25:04 -0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "É vegano?",
   description: "Analisador de produtos veganos",
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>',
+  },
 };
 
 export default function RootLayout({
